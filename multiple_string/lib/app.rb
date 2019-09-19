185d6621fd422d0091c089905487524f4454ba27
kenzo2013 <kkenzo2007@yahoo.fr>
2019-09-19 15:15:26 +0200

    multiple string

---
+++ multiple_string/lib/app.rb
@@ -0,0 +1,4 @@
+require_relative '../lib/multiple_string'
+
+query = MultipleString.new("a", 10)
+query.call
---
+++ multiple_string/lib/multiple_string.rb
@@ -0,0 +1,20 @@
+class MultipleString
+  attr_reader :str, :num
+  def initialize(str:, num:)
+    @str = str
+    @num = num
+  end
+
+  def call
+    print_string
+  end
+
+
+
+  def print_string
+    return if num <= 0
+    1..num.each do |i|
+      puts str.to_s * i
+    end
+  end
+end
