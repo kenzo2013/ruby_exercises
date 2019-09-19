d26a2e7aeb966078288237aa68af8f490df08993
kenzo2013 <kkenzo2007@yahoo.fr>
2019-09-19 15:33:59 +0200

    multiple_string: add spec

--- multiple_string/lib/multiple_string.rb
+++ multiple_string/lib/multiple_string.rb
@@ -1,6 +1,6 @@
 class MultipleString
   attr_reader :str, :num
-  def initialize(str:, num:)
+  def initialize(str, num)
     @str = str
     @num = num
   end
@@ -13,8 +13,9 @@ class MultipleString
 
   def print_string
     return if num <= 0
-    1..num.each do |i|
+    (1..num).each do |i|
       puts str.to_s * i
     end
   end
 end
+
---
+++ multiple_string/spec/multiple_string_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/multiple_string'
+
+describe MultipleString do
+
+  describe '#print_string' do
+    let(:str) { "a" }
+    it 'multiple string' do
+      expect("a" * 2).to eq "aa"
+    end
+  end
+end
